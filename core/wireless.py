6e580731826d468e7f60d9c18b1a046bdf9a7405
6A <78461864+6A-Realm@users.noreply.github.com>
2022-03-11 18:12:15 -0800

    V 1.0.0
    Fully working model.

--- core/wireless.py
+++ core/wireless.py
@@ -1,15 +1,12 @@
 from disnake.ext import commands
-from yaml import load
+from yaml import safe_load
 import asyncio
 from rich.console import Console
 console = Console()
 
-# List of cogs
-cogs = ["error", "image", "miscellaneous", "remote", "screen"]
-
 # Loads switch ip and port from config file
 with open("config.yaml") as file:
-    data = load(file)
+    data = safe_load(file)
     switchip = data["ip"]
     switchport = data["port"]
 
@@ -27,29 +24,6 @@ class connection(commands.Cog):
             await self._w.drain()
         except Exception as e: 
             console.print(f"Unable to send commands to switch. {e}", style="red")
-
-    # Ping switch, fetch title ID, check if auto screen off
-    async def initiate(self):
-        try:
-            self._r, self._w = await asyncio.open_connection(switchip, switchport, limit = 524288)
-            console.print(f"Successfully connected to {switchip}:{switchport}.", style="green")
-
-            # Set controller
-            await self.switch("detachController")
-            await self.switch("controllerType 1")
-
-            # Close socket
-            self._w.close()
-            await self._w.wait_closed()
-            
-            for e in cogs:
-                try:
-                    self.syscord.load_extension("cogs." + cogs)
-                except Exception as err:
-                    console.print(f"Unable to load {e} {err}.", style="red")
-        except:
-            console.print(f"Unable to connect to {switchip}:{switchport}.", style="red")
-            console.print("Click here to follow the connection troubleshooting guide: https://github.com/6A-Realm/SysBot.py/wiki/Connection-Issues", style="yellow")
 
     # IP/Port connection to switch (same thing you would put in sysbot)
     async def connect(self):
--- main.py
+++ main.py
@@ -1,22 +1,68 @@
 # Imports 
-import disnake
+import disnake, asyncio
 from disnake.ext import commands
-from yaml import load, warnings
-import core.wireless as s
+from yaml import safe_load
+from rich.console import Console
+console = Console()
 
 # Load config.yaml
 with open("config.yaml") as file:
-    data = load(file)
+    data = safe_load(file)
     token = data["token"]
     botprefix = data["prefix"]
+    switchip = data["ip"]
+    switchport = data["port"]
 
 # Defining the bot and settings
-syscord = commands.Bot(name="SysCord - V.0.1.0", command_prefix = botprefix, help_command = None, activity = disnake.Game(name="SysCord"), intents = disnake.Intents.all())
-warnings({'YAMLLoadWarning': False})
+syscord = commands.Bot(name="SysCord - V.1.0.0", command_prefix = botprefix, help_command = None, activity = disnake.Game(name="SysCord"), intents = disnake.Intents.all())
 
-# Connect and initiate connection
-syscord.load_extension("core.wireless")
-s.connection(syscord).initiate()
+# Ping switch, set controller, load cogs
+async def initiate(syscord):
+
+    # List of cogs
+    cogs = str["error", "image", "miscellaneous", "remote", "screen"]
+
+    try:
+        reader, writer = await asyncio.open_connection(switchip, switchport)
+        console.print(f"Successfully connected to {switchip}:{switchport}.", style="green")
+
+        # Set controller
+        try:
+            await writer.write("detachController\r\n".encode())
+            await writer.drain()
+        except:
+            print("1")
+        try:
+            await writer.write("controllerType 1\r\n".encode())
+            await writer.drain()
+        except:
+            print("2")
+
+        try:
+            # Close socket connection
+            writer.close()
+            await writer.wait_closed()
+        except:
+            print("3")
+
+        try:
+            # Load cogs
+            for e in cogs:
+                try:
+                    syscord.load_extension("cogs." + cogs)
+                except Exception as err:
+                    console.print(f"Unable to load {e} {err}.", style="red")
+        except:
+            print("4")
+    except:
+        console.print(f"Unable to connect to {switchip}:{switchport}.", style="red")
+        console.print("Click here to follow the connection troubleshooting guide: https://github.com/6A-Realm/SysBot.py/wiki/Connection-Issues", style="yellow")
+
+#Debugger
+syscord.load_extension('jishaku')
+
+# Start all
+asyncio.run(initiate(syscord))
 
 try:
     syscord.run(token)
